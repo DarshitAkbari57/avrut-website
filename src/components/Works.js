466dc780a36524c211fc913673cc8b6dda41719c
Darshankhokhariya <darshankhokhariya26@gmail.com>
2023-05-30 16:59:55 +0530

    json change

--- src/components/Works.js
+++ src/components/Works.js
@@ -1,17 +1,32 @@
 
 const Works = [
-
-    {
-        name:'Alphadog',
-        language:'React js',
-        detail:'It is the premier virtual destination for greyhound racing in metaverse.',
-        Image:require('../images/alphaDog.png'),
-    },
     {
         name:'dematade',
         language:'Next js',
         detail:'It is a full automatic robo trading to make your trading strategies  & market.',
         Image:require('../images/dematade.png'),
+    },
+    {
+        name:'Haat',
+        language:'Next js',
+        detail:'It is a food-delivery Application For israel.',
+        Image:require('../images/haat.png'),
+    },
+    {
+        name:'vida',
+        language:'Next js',
+        detail:'vida modernizes lung and respiratory care, empowering healthcare organizations and physicians.',
+        Image:require('../images/vida.png'),
+    }, {
+        name:'maseEntertainment',
+        language:'Next js',
+        detail:'It is providing the hottest sound and the best entertainment in and around the saint louis area.',
+        Image:require('../images/maseEntertainment.png'),
+    },{
+        name:'lybrate',
+        language:'Next js',
+        detail:'Lybrate is an online doctor consultation and appointment booking.',
+        Image:require('../images/lybrate.png'),
     },
     {
         name:'fanguru',
@@ -25,36 +40,17 @@ const Works = [
         detail:'It is an online accessories shopping website.',
         Image:require('../images/fashionphile.png'),
     },
+   
+   
+  
     {
         name:'glamsquad',
         language:'Next js',
         detail:'it is a beauty care booking and consultation portal.',
         Image:require('../images/glamsquad.png'),
     },
-    {
-        name:'lybrate',
-        language:'Next js',
-        detail:'Lybrate is an online doctor consultation and appointment booking.',
-        Image:require('../images/lybrate.png'),
-    },
-    {
-        name:'maseEntertainment',
-        language:'Next js',
-        detail:'It is providing the hottest sound and the best entertainment in and around the saint louis area.',
-        Image:require('../images/maseEntertainment.png'),
-    },
-    {
-        name:'vida',
-        language:'Next js',
-        detail:'vida modernizes lung and respiratory care, empowering healthcare organizations and physicians.',
-        Image:require('../images/vida.png'),
-    },
-    {
-        name:'Haat',
-        language:'Next js',
-        detail:'It is a food-delivery Application For israel.',
-        Image:require('../images/haat.png'),
-    },
+    
+   
     {
         name:'Acheti',
         language:'Next js',
@@ -67,7 +63,13 @@ const Works = [
         detail:'It is a property buying and selling portal.',
         Image:require('../images/acheti.png'),
     },
-    
+    {
+        name:'Alphadog',
+        language:'React js',
+        detail:'It is the premier virtual destination for greyhound racing in metaverse.',
+        Image:require('../images/alphaDog.png'),
+    },
+   
     
     
    
